10765cfeecd83c2008f5848ec6c85156e4a24d69
Fritzz-zz <41195041+Fritzz-zz@users.noreply.github.com>
2019-12-09 19:51:10 +0800

    Level 2 Baked and Post processing applied

--- Final Project/Assets/Scripts/PickUpScripts/BoxPickUpScript.cs
+++ Final Project/Assets/Scripts/PickUpScripts/BoxPickUpScript.cs
@@ -4,8 +4,33 @@ using UnityEngine;
 
 public class BoxPickUpScript : MonoBehaviour
 {
+    /*Vector3 objectPos;*/
     public Transform theDest;
+    /*public GameObject box;
+    public bool isHolding = false;*/
     // Start is called before the first frame update
+
+    /*private void Update()
+    {
+        if (isHolding == true)
+        {
+            box.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            box.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            box.transform.SetParent(theDest.transform);
+
+           
+        }
+        else
+        {
+            objectPos = box.transform.position;
+            box.transform.SetParent(null);
+            box.GetComponent<Rigidbody>().useGravity = true;
+            box.transform.position = objectPos;
+
+
+        }
+    }*/
+
     void OnMouseDown()
     {
        
@@ -15,6 +40,7 @@ public class BoxPickUpScript : MonoBehaviour
         GetComponent<Rigidbody>().useGravity = false;
         this.transform.position = theDest.position;
         this.transform.parent = GameObject.Find("Destination").transform;
+        /*isHolding = true;*/
 
     }
 
@@ -27,6 +53,6 @@ public class BoxPickUpScript : MonoBehaviour
         GetComponent<Rigidbody>().detectCollisions = true;
         /*GetComponent<CapsuleCollider>().enabled = true;*/
         /*GetComponent<MeshCollider>().enabled = true;*/
-
+        /*isHolding = false;*/
     }
 }

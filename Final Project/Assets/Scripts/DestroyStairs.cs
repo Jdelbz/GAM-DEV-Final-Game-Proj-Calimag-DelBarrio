5580e4783008043a5fd16d7be12b09161e155033
Fritzz-zz <41195041+Fritzz-zz@users.noreply.github.com>
2019-12-13 04:58:57 +0800

    Initial level 3 design

---
+++ Final Project/Assets/Scripts/DestroyStairs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyStairs : MonoBehaviour
+{
+    public GameObject Stairs;
+    public bool active = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active == false)
+        {
+            Object.Destroy(Stairs);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            active = false;
+        }
+        else if (other.tag == "BoxKey")
+        {
+            active = true;
+        }
+    }
+}
---
+++ Final Project/Assets/Scripts/StairsAppear.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairsAppear : MonoBehaviour
+{
+    public GameObject Stairs;
+    public bool active = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active == true)
+        {
+            Stairs.SetActive(true);
+        }
+        else
+        {
+            Stairs.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            active = false;
+        }
+        else if (other.tag == "BoxKey")
+        {
+            active = true;
+        }
+    }
+}
